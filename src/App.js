c0c5d7779f673dd93fbcb15f7a7f015ec5591080
dad0586 <otaboy0586@gmail.com>
2024-10-09 15:37:47 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useState } from 'react'
+import "./assets/style/index.scss"
+
+const App = () => {
+  return (
+    <>
+      <div className='changed'>
+        <h1> My name</h1>
+        <button >  Change name</button>
+      </div>
+    </>
+  )
+}
+
+export default App
