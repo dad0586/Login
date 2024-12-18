cc2a56ad6888007fe1393646d0bdcdac52e001b9
otaboy <user@MacBook-Pro-user.local>
2024-12-18 21:21:30 +0500

    new

--- src/App.js
+++ src/App.js
@@ -2,15 +2,21 @@ import React from 'react'
 import { useState } from 'react'
 import "./assets/style/index.scss"
 
-const App = () => {
-  return (
-    <>
+
+function Mycomponent() {
+      const [ Name, setName] = useState("Mirjalol");
+  
+  const updatename = () =>{
+    setName("Otaboy")
+  }
+    return (
+      <>
       <div className='changed'>
-        <h1> My name</h1>
-        <button >  Change name</button>
+        <h1> My name:{Name} </h1>
+        <button onClick={updatename} >  Change name</button>
       </div>
     </>
   )
 }
 
-export default App
+export default Mycomponent
